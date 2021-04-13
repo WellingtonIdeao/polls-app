d55892982d2197f43a7dffdb5772508d28d92be3
Wellington Ideao <wellington.ideao@gmail.com>
2021-04-12 21:59:18 -0300

    Update views.py with generic views class

--- polls/views.py
+++ polls/views.py
@@ -3,42 +3,27 @@ from django.template import loader
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 # Create your views here.
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    '''
-        forma longa de carregar um template, preencher um contexto e retornar um HttpResponse object
-        
-        template = loader.get_template(template_name)
-        return HttpResponse(template.render(context, request))
-    '''
-    # usando um atalho
-    return render(request, template_name, context)
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        # Return the last five published questions.
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    ''''
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist')
-    '''
-
-    # atalho para usar get() e raise Http404 se o objeto não existir
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question},)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question, }
-    return render(request, 'polls/results.html', context)
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
